c588f357e5ccaa097b116bcfa0d3ac07c2d1973f
José L. Flores (Laptop Win-11) <joseluisfloresquisbert@gmail.com>
2023-05-31 03:20:59 -0400

    Desarrollo del index principal del sistema

--- Components/footer.php
+++ Components/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-primary text-center text-white">
+<footer class="bg-primary text-center text-white sticky-bottom">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         &copy; STIS <?php echo date('Y');?>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv= 0  content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contabilidad</title>
+    <!-- BOOTSTRAP -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <!-- Toggle button -->
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+        <i class="fas fa-bars"></i>
+        </button>
+
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Navbar brand -->
+            <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                CONTABILIDAD
+            </a>
+        <!-- Left links -->
+        </div>
+        <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
+    <div class="container mt-4 mb-4">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Cuentas</h5>
+                        <p class="card-text">Gestione las cuentas contables de su negocio.</p>
+                        <a href="cuentas/index.php" class="btn btn-primary">Ir a cuentas</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Comprobantes</h5>
+                        <p class="card-text">Registrar comprobantes de las transacciones de su negocio.</p>
+                        <a href="comprobantes/index.php" class="btn btn-primary">Registrar comprobantes</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+</html>
