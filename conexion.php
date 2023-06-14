e367124fc989ecc129a23ad748132d2c4ea517e6
Jiuseppe STIS Bolivia <joseluisfloresquisbert@gmail.com>
2023-06-14 18:22:11 -0400

    .gitignore environments

---
+++ conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+    // VERIFICACIÓN DE CONEXIÓN A LA BASE DE DATOS
+    require('php/environments.php');
+    // CONEXION CON LA BASE DE DATOS
+    if(isset($_SESSION['base']) && isset($_SESSION['server'])){
+        $server = $_SESSION['server'];
+        $database = $_SESSION['base'];
+    }else{
+        $server = $_COOKIE['server'];
+        $database = $_COOKIE['base'];
+    }
+    $user_db = $USER_DB;
+    $pass_db = $PASS_DB;
+    $char_set = $CHARACTER_SET;
+
+    $connection_info = array(
+        "Database" => $database,
+        "Uid" => $user_db,
+        "PWD" => $pass_db,
+        "CharacterSet" => $char_set
+    );
+    $con = sqlsrv_connect( $server , $connection_info );
+    if( !$con ){
+        echo 'No puede conectarse con la base de .';
+    }
+    
+?>
