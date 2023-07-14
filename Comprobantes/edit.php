7f33776fe319096774a016514ede74714c718bf4
Ruon2414 <112769866+Ruon2414@users.noreply.github.com>
2023-07-14 11:12:19 -0400

    local

--- Comprobantes/edit.php
+++ Comprobantes/edit.php
@@ -50,17 +50,32 @@
 <!DOCTYPE html>
     <html lang="es">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv= 0  content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Comprobantes</title>
-        <!-- BOOTSTRAP -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-        <link href="../css/select2.min.css" rel="stylesheet" />
-        <script src="../js/jquery.min.js"></script>
-        <link rel="stylesheet" href="../css/bootstrap-table.min.css">
-        <script src="../js/bootstrap-table.min.js"></script>
+      <meta charset="utf-8">
+      <meta http-equiv= 0  content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Comprobantes</title>
+      <!-- BOOTSTRAP -->
+      <link href="../css/bootstrap.min.css" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+      <link href="../css/select2.min.css" rel="stylesheet" />
+      <script src="../js/jquery.min.js"></script>
+      <link rel="stylesheet" href="../css/bootstrap-table.min.css">
+      <script src="../js/bootstrap-table.min.js"></script>
+      <style>
+        .btn-circle {
+          width: 30px;
+          height: 30px;
+          padding: 6px 0px;
+          border-radius: 15px;
+          text-align: center;
+          font-size: 12px;
+          line-height: 1.42857;
+        }
+        .btn-circle i{
+          line-height: 1rem;
+          font-size: 19px;
+        }
+      </style>
     </head>
     <body>
         <?php
@@ -154,14 +169,15 @@
                         </div>
                       </div>
                       <div class="row mt-2">
-                        <div class="col-md-5">
+                        <div class="col-md-4">
                           <input type="file" class="form-control" id="pdfFile" accept=".pdf" name="pdfFile"/>
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-4">
                           <label class="help"><?php echo $filePdf != null && $filePdf != 'no' ? 'Existe archivo adjunto | <b>'.$filePdf .' </b>': 'No hay archivo adjunto';  ?></label>
                           <input type="hidden" id="namePDF" value="<?php echo $filePdf != null && $filePdf != 'no' ? $filePdf : 'x';  ?>">
                         </div>
-                        <div class="col-md-5">
+                        <div class="col-md-4">
+                          <button class="btn btn-primary" onclick="addAsiento()"><i class="bi bi-plus me-2"></i>Adicionar Asiento</button>
                         </div>
                       </div>
                       <div class="row mt-2">
@@ -178,6 +194,7 @@
                                 <th scope="col">HABER ($us)</th>
                                 <th scope="col">BCO.</th>
                                 <th scope="col">CHEQUE</th>
+                                <th scope="col"> X </th>
                               </tr>
                             </thead>
                             <tbody id="asientos">
@@ -223,6 +240,9 @@
                                   <div class="form-outline">
                                     <input type="text" id="cheque-<?php echo $cantidad?>" name="cheque[]" class="form-control" value="<?php echo $asiento['cheque']; ?>">
                                   </div>
+                                </td>
+                                <td>
+                                  <button type="button" class="btn btn-danger btn-circle" onclick="eliminarAsiento(<?php echo $cantidad?>)"><i class="bi bi-x-circle"></i></button>
                                 </td>
                               </tr>
                             <?php
